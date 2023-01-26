7a8d19a0fa444145275bbfdc8b2744cfa017bae8
Haikal690 <117075548+Haikal690@users.noreply.github.com>
2023-01-26 14:48:59 +0800

    Create Q2client.py

---
+++ Q2client.py
@@ -0,0 +1,17 @@
+import socket
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('192.168.100.18', 8080))
+
+    fahren = float(input("Enter temperature in Fahrenheit: "))
+    client_socket.send(str(fahren).encode())
+
+    cels = client_socket.recv(1024).decode()
+    print(f"Temperature in Celsius: {cels}")
+
+    client_socket.close()
+
+if __name__ == '__main__':
+    main()
+
